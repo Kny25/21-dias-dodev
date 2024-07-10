1ca78cc51173069346f43b697e98d661ef734cac
unknown <kennedyraze@gmail.com>
2024-07-09 22:11:51 -0300

    subindo exercicios da semana

---
+++ dia 16-dodev/ex fixaão/index.js
@@ -0,0 +1,69 @@
+//////// criação das classes, funções e variaveis do projeto////
+let Array = [];
+let control = true;
+
+class Fix {
+    nome
+    idade
+    altura
+    constructor(nome, idade, altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+}
+
+function OrdernaIdade(arr) {
+    arr.sort((a, b) => a.idade - b.idade)
+}
+function OrdenarPorAltura(arr) {
+    arr.sort((a, b) => a.altura - b.altura)
+    return arr;
+}
+function OrdenarPorNome(arr) {
+    arr.sort((a, b) => a.nome.localeCompare(b.nome))
+    return arr;
+}
+let tot = 0
+function MediaAltura(arr){
+    for(let i = 0;i < arr.length;i ++){
+     tot += arr[i].altura + arr[i].altura
+    }
+   return tot / arr.length;
+}
+function MediaIdade(arr){
+    for(let i = 0;i < arr.length;i ++){
+     tot += arr[i].idade + arr[i].idade
+    }
+   return tot / arr.length;
+}
+
+/////// estrutura do codigo/////
+while (control) {
+    let nam = prompt("digite seu nome")
+    let age = Number(prompt("digite sua idade"))
+    let heigth = parseFloat(prompt("digite sua altura"))
+    Array.push(new Fix(nam, age, heigth))
+    let inserir = prompt("deseja inserir outro nome ?(s/n)")
+    if (inserir != "s") {
+        break;
+    }
+}
+
+let ordenar = prompt("deseja ordenar por que ordem os alunos ?")
+
+
+/////criando fluxo de funcionamento do codigo//////
+switch (ordenar) {
+    case "idade":
+        OrdernaIdade(Array)
+        break;
+    case "altura":
+        OrdenarPorAltura(Array)
+        break;
+    case "nome":
+        OrdenarPorNome(Array)
+        break;
+}
+console.log("a media de altura dos participantes é de " + MediaAltura(Array))
+console.log("a media de idade dos participantes é de " + MediaIdade(Array))
